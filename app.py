be65d931a666ff0586fdcaa1329a78c94447ba78
launda <vinorda@gmail.com>
2018-08-29 23:36:22 +1000

    Created a detail page about each airport.

--- app.py
+++ app.py
@@ -8,37 +8,38 @@ cur_dir = os.path.dirname(__file__)
 
 app = Flask(__name__)
 
+
+def get_csv():
+	airports = get_avlocs().reset_index()
+	# get only those in VIC
+	airports = airports.loc[airports['State'].str.contains('VIC'),]
+
+    # convert DF .to_dict() as we can't pass pandas DF to jinja2 templates
+	# 'records' works as it gets rid of row index values for each column
+
+    # another option is convert DF .to_html()
+    # But less html control as it converts as whole blocl to html
+	with open(os.path.join(cur_dir,'templates', 'airports.html'), 'w') as ob:
+		airports.to_html(ob,bold_rows=True,
+            border=4, col_space=10,justify='right',escape=False)
+
+	airports = airports.to_dict(orient='records')
+	return airports
+
+
 @app.route("/")
 def index():
     template = 'index.html'
-    airports = get_avlocs().reset_index()
-    # get only those in VIC
-    airports = airports.loc[airports['State'].str.contains('VIC'),]
-
-    '''Now we can't pass a dataframe to jinja2 template '''
-    # 1st Solution - convert to html - but little control over display
-    '''
-    pd.set_option('display.max_columns', None)
-    with open(os.path.join(cur_dir,'templates', 'airports.html'), 'w') as ob:
-        airports.to_html(ob,bold_rows=True,
-            border=4, col_space=10,justify='right',escape=False)
-    '''
-    
-    # 2nd solutin - Convert the DataFrame to a dictionary.
-    '''
-    DataFrame.to_dict(orient='dict', into=<class 'dict'>)[source]
-    orient : str {‘dict’, ‘list’, ‘series’, ‘split’, ‘records’, ‘index’}
-    Determines the type of the values of the dictionary.
-    ‘dict’ (default) : dict like {column -> {index -> value}}
-    ‘list’ : dict like {column -> [values]}
-    ‘series’ : dict like {column -> Series(values)}  <--closest
-    ‘records’ : list like [{column -> value}, … , {column -> value}]
-    '''
-    # 'records' works as it gets rid of row index values for each column
-    airports = airports.to_dict(orient='records')
-
-
+    airports = get_csv()
     return render_template(template, object_list=airports)
+
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['LOC_ID'] == row_id:
+            return render_template(template, object=row)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
