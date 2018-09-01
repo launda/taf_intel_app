afbf823ac9d80ca358abfd2cb848a3595c323aa4
launda <vinorda@gmail.com>
2018-09-01 13:42:40 +1000

    Add pop ups on maps

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import abort
+from flask import abort # jsonify - no need 
 from flask import render_template
 import os
+# import json  - no needed
 
 from avlocs import get_avlocs
 
@@ -19,16 +20,24 @@ def get_airport_csv():
 
     # another option is convert DF .to_html()
     # But less html control as it converts as whole block to html
+    '''
     with open(os.path.join(cur_dir,'templates', 'airports.html'), 'w') as ob:
         airports.to_html(ob,bold_rows=True,
             border=4, col_space=10,justify='right',escape=False)
-
+    '''
     '''
     df.to_dict() converts df into dict, so columns like 'Location', 'Area'
     are translated into “keys” on dictionary objects 
     that you can access like airports['Location'].
     '''
+    # grab 1st 8 cols and dropna() - other cols MSA and SAM have many nans
+    # will drop almost all airport!!
+    print("Airports shape b4 drop na", airports.shape)
+    airports = airports.iloc[:,:8].dropna()
+    print(airports.head())
+    print("isnull().any()", airports.isnull().any())
     airports = airports.to_dict(orient='records')
+    print("Airports shape after drop na", len(airports))  #dict has no shape
     return airports
 
 '''
@@ -43,7 +52,7 @@ an HTML table that lists all airport details
 def index():
     template = 'index.html'  # html page to render
     airports = get_airport_csv()     # get airport data as a dict obj
-    print (airports[:2])     # list of dict entries - each dict is one row from df
+    print (airports[:1])     # list of dict entries - each dict is one row from df
     '''
     [{'LOC_ID': 'YMAY', 'AREA': 22.0, 'Lat': -36.0676, 'Long': 146.9582, 'Type': 'AD', 'Reg': 'YM',
      'State': 'NSW', 'Location': 'Albury', 'HAM (cld (ft))': 2231, 'HAM (vis (m))': 7000, 
@@ -52,8 +61,21 @@ def index():
      'State': 'NSW', 'Location': 'Armidale', 'HAM (cld (ft))': 1384, 'HAM (vis (m))': 6000, 
      'SAM (cld (ft))': nan, 'SAM (vis (m))': nan, ' MSA (ft)': nan}]
     '''
-    return render_template(template, object_list=airports)
+    '''https://stackoverflow.com/questions/7907596/json-dumps-vs-flask-jsonify
+    jsonify() function in flask returns a flask.Response() object that already has 
+    the appropriate content-type header 'application/json' for use with json responses.
 
+    obj_jsonify = jsonify(airports)
+    print(obj_jasonify)   # <Response 44428 bytes [200 OK]> what da hell
+
+    json.dumps() method will just return an encoded string, 
+    which would require manually adding the MIME type header.
+
+    obj_json_dump = json.dumps(airports)
+    print(obj_json_dump[:10])  # iterrates by a char at a time !!! 
+    don't try with jsonify as its not iterable!! '''
+
+    return render_template(template, object_list=airports) 
 
 
 '''
@@ -65,12 +87,12 @@ to pull the record with the corresponding id from the dict.
 When we find a match, pass that row out to the template for 
 rendering with the name object on its own unique page.
 '''
-@app.route('/<string:row_id>/')
+@app.route('/aero_details/<string:row_id>/')
 def detail(row_id):
     template = 'detail.html'  # the html page to render content to 
     object_list = get_airport_csv()   # get data as list of records
     for row in object_list:   # find record/row for given airport code 
-        if row['LOC_ID'] == row_id:
+        if row['LOC_ID'] == row_id.upper():
             return render_template(template, object=row)
     abort(404)  # Display MSG if no page found
     #Not Found : The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
